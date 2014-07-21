13bc15dece2d03a7a527c727aafb69ecbe3d2e94
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-07-21 18:27:19 +0800

    Change ElementExample to debug folding issue

--- ElementExample.elm
+++ ElementExample.elm
@@ -4,6 +4,7 @@ import Test
 import ElmTest.Run as Run
 import ElmTest.Runner.Element as Element
 import ElmTest.Runner.String  as String
+import ElmTest.Test (..)
 
 prettyOut : Signal Element
 prettyOut = let x = Element.runDisplay Test.suite3
@@ -15,5 +16,25 @@ uglyOut = String.runDisplay Test.suite2
 uglyOut' : String
 uglyOut' = String.runDisplay Test.suite3
 
-main : Signal Element
-main = prettyOut
+--main : Signal Element
+--main = prettyOut
+
+main = let one = Test.suite3
+           two = runna one
+           three = runna two
+           four = runna three
+           five = runna four
+           six = runna five
+       in  flow right <| map (\x -> showTest x `above` spacer 50 50) [one, two, three, four, five, six]
+
+runna : Test -> Test
+runna = fromJust . snd . Run.runOne
+
+showTest : Test -> Element
+showTest t =
+    case t of
+        TestCase name _ -> asText name
+        Suite name ts   -> flow down <| asText name :: map (\x -> flow right [spacer 10 10, showTest x]) ts
+
+fromJust : Maybe a -> a
+fromJust (Just x) = x
