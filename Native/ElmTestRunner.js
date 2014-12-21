bb6b0caa97fbb82718781eedb9119c308c7a3bb9
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-12-21 18:34:42 +0800

    Forgot to track Native runner!

---
+++ Native/ElmTestRunner.js
@@ -0,0 +1,26 @@
+Elm.Native.ElmTestRunner = {};
+Elm.Native.ElmTestRunner.make = function(elm) {
+    elm.Native = elm.Native || {};
+    elm.Native.ElmTestRunner = elm.Native.ElmTestRunner || {};
+    if (elm.Native.ElmTestRunner.values) {
+        return elm.Native.ElmTestRunner.values;
+    }
+
+    var Utils = Elm.Native.Utils.make(elm);
+
+    function runAssertion(assertion, a, b) {
+        try {
+            var result = assertion(Utils.Tuple0);
+            var msg = "Expected: " + a(Utils.Tuple0) + "; got: " + b(Utils.Tuple0);
+        } catch (e) {
+            var result = false;
+            var msg = e.toString();
+        }
+        return Utils.Tuple2(result, msg);
+    }
+
+    return elm.Native.ElmTestRunner.values = {
+        runAssertion : F3(runAssertion)
+    };
+
+};
